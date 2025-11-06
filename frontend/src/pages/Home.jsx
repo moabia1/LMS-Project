53b1275edac66d71dab278f23c07f41d6291a23f
moabia1 <mdmoabia81@gmail.com>
2025-11-06 17:37:40 +0530

    complete home section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import home from "../assets/home1.jpg";
+import { SiViaplay } from "react-icons/si";
+import searchIcon from "../assets/ai.png"
+import searchIcon1 from "../assets/SearchAi.png"
 
 const Home = () => {
   return (
-    <div className='w-full overflow-hidden'>
-      <div className='w-full lg:h-[140vh] h-[70vh] relative'>
-        <Navbar/>
+    <div className="w-full overflow-hidden">
+      <div className="w-full lg:h-[140vh] h-[70vh] relative">
+        <Navbar />
+        <img
+          src={home}
+          className="object-cover md:object-fill w-full lg:h-full h-[50vh] "
+          alt=""
+        />
+
+        <span className="lg:text-[70px] absolute md:text-[40px] lg:top-[10%] top-[15%] w-full flex items-center justify-center text-white font-bold text-[20px]">
+          Grow Your Skills to Advance
+        </span>
+        <span className="lg:text-[70px] absolute md:text-[40px] lg:top-[18%] top-[20%] w-full flex items-center justify-center text-white font-bold text-[20px]">
+          Your Career Path
+        </span>
+
+        <div className="absolute lg:top-[30%] top-[75%] md:top-[80%] w-full flex items-center justify-center gap-3 flex-wrap">
+          <button className="px-5 py-2.5 border-2 lg:border-white border-black lg:text-white text-black rounded-[10px] text-[18px] font-light flex gap-2 cursor-pointer">
+            View All Courses <SiViaplay className="w-[30px] h-[30px] lg:fill-white fill-black"/>
+          </button>
+          <button className="px-5 py-2.5 lg:bg-white bg-black lg:text-black text-white rounded-[10px] text-[18px] font-light flex gap-2  cursor-pointer items-center justify-center">
+            Search With AI <img src={searchIcon} className="w-[30px] h-[30px] rounded-full hidden lg:block" alt="" /> <img src={searchIcon1} className="w-[35px] h-[35px] rounded-full lg:hidden" alt="" />
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
