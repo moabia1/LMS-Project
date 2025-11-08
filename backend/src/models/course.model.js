9819559fbda6bafb8e0e823c8d3a274e3d8443fe
moabia1 <mdmoabia81@gmail.com>
2025-11-09 01:34:44 +0530

    Completed course model

--- backend/src/models/course.model.js
+++ backend/src/models/course.model.js
@@ -1,1 +1,52 @@
 import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required:true
+  },
+  subTitle: {
+    type:String
+  },
+  description: {
+    type:String
+  },
+  category: {
+    type: String,
+    required:true
+  },
+  level: {
+    type: String,
+    enum:["beginner","intermediate","advanced"]
+  },
+  price: {
+    type:String
+  },
+  thumbnail: {
+    type:String
+  },
+  enrolledStudent: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  }],
+  lectures: [{
+    type:mongoose.Schema.Types.ObjectId
+  }],
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  },
+  isPublished: {
+    type: Boolean,
+    default:false
+  },
+  reviews: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Review"
+  }]
+}, { timestamps: true })
+
+
+const Course = mongoose.model("Course", courseSchema);
+
+export default Course
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema(
     enrolledCourses: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Courses",
+        ref: "Course",
       },
     ],
     role: {
